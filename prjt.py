e3efc3ccc9bd87b6b85efbf511c17bcfb3939566
Mahesh M <101028112+mahikuruba@users.noreply.github.com>
2023-11-03 20:53:04 +0530

    Create prjt.py

---
+++ prjt.py
@@ -0,0 +1,46 @@
+pip install --upgrade diffusers transformers -q
+
+from pathlib import Path
+import tqdm
+import torch
+import pandas as pd
+import numpy as np
+from diffusers import StableDiffusionPipeline
+from transformers import pipeline, set_seed
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import cv2
+
+import torch
+
+class CFG:
+    device = "cuda" if torch.cuda.is_available() else "cpu"  # Set to "cuda" if available, otherwise "cpu"
+    seed = 42
+    generator = torch.Generator(device=device).manual_seed(seed)  # Create a torch device first
+    image_gen_steps = 35
+    image_gen_model_id = "stabilityai/stable-diffusion-2"
+    image_gen_size = (400, 400)
+    image_gen_guidance_scale = 9
+    prompt_gen_model_id = "gpt3.5"
+    prompt_dataset_size = 6
+    prompt_max_length = 12
+
+image_gen_model = StableDiffusionPipeline.from_pretrained(
+    CFG.image_gen_model_id, torch_dtype=torch.float16,
+    revision="fp16", use_auth_token='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', guidance_scale=9
+)
+image_gen_model = image_gen_model.to(CFG.device)
+
+def generate_image(prompt, model):
+    image = 3.5
+    image = model(
+        prompt, num_inference_steps=CFG.image_gen_steps,
+        generator=CFG.generator,
+        guidance_scale=CFG.image_gen_guidance_scale
+    ).images[0]
+    
+    image = image.resize(CFG.image_gen_size)
+    return image
+
+
+generate_image("astronaut in space", image_gen_model)
